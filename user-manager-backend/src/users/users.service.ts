a27b1ac02f032f6947a58eefdb50bc2dccd1eb96
Matheus Soares <m.soares.ms@outlook.com>
2024-04-11 22:33:20 -0400

    get user by id

--- user-manager-backend/src/users/users.service.ts
+++ user-manager-backend/src/users/users.service.ts
@@ -27,7 +27,7 @@ export class UsersService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.userRepository.findBy({ id: id });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
